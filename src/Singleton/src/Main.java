6753b5c4177430eac086ef7174a13a1768af74e6
dogopogo <shoebilloftheyear@gmail.com>
2019-05-12 11:03:36 +0900

    Singletonを実装

---
+++ src/Singleton/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Start.");
+		Singleton obj1 = Singleton.getInstance();
+		Singleton obj2 = Singleton.getInstance();
+		if (obj1 == obj2) {
+			System.out.println("同じインスタンス");
+		} else {
+			System.out.println("違うインスタンス");
+		}
+		System.out.println("End.");
+
+	}
+
+}
